36c3026a1b91887084f512263b09e80b177e3fe9
Chris Rendle-Short <chris@killred.net>
2026-01-19 19:27:12 +1100

    Piantor: Add fwd/back to second layer, move backslash to home row

--- keyboards/beekeeb/piantor/keymaps/vial/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/vial/keymap.c
@@ -26,9 +26,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [1] = LAYOUT_split_3x6_3(
-        KC_GRV,  KC_1,    KC_2,    KC_3,       KC_4,       KC_5,                      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,          KC_MPLY,
-        KC_TRNS, KC_MINS, KC_EQL,  KC_LBRC,    KC_RBRC,    KC_NO,                     KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, LSFT(KC_BSLS), KC_VOLU,
-        KC_TRNS, MO(2),   KC_NO,   LSFT(KC_9), LSFT(KC_0), KC_NO,                     KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   KC_BSLS,       KC_VOLD,
+        KC_GRV,  KC_1,    KC_2,    KC_3,       KC_4,             KC_5,                KC_6,     KC_7,     KC_8,     KC_9,     KC_0,          KC_MPLY,
+        KC_TRNS, KC_MINS, KC_EQL,  KC_LBRC,    KC_RBRC,          KC_NO,               KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_BSLS,       KC_VOLU,
+        KC_TRNS, MO(2),   KC_NO,   LGUI(KC_LBRC), LGUI(KC_RBRC), KC_NO,               KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   KC_NO,         KC_VOLD,
                                             KC_TRNS,  KC_NO,  KC_TRNS,       KC_TRNS, KC_TRNS,  KC_TRNS
     ),
 
